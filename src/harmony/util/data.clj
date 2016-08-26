fe6e577a5bfec152cc82dce245d8e1af6293c911
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2016-08-26 22:02:42 +0300

    add data util from search-api + map-keys

---
+++ src/harmony/util/data.clj
@@ -0,0 +1,76 @@
+(ns harmony.util.data
+  "Utilities for working with Clojure data structures.")
+
+;; assoc, dissoc and update for relational data modelled as a seq of
+;; maps (called xrel by functions in clojure.set namespace). See tests
+;; in harmony.util.data-test for examples of how to use these
+;; functions.
+
+(defn- vals-to-flat-coll [indexed to]
+  (let [res (->> indexed vals (apply clojure.set/union))]
+    (if (set? to)
+      res
+      (into to res))))
+
+(defn assoc-indexed
+  "Returns a new set of relations created by adding given val-set to
+  xrel. If the selector matches a set of indexed values indexed by ks
+  using clojure.set/index in xrel then the returned set has the
+  previous relations matching the selector replaced with those in
+  val-set. If the selector doesn't match any previous values then the
+  returned set is a superset of xrel."
+  [xrel ks selector val-set]
+  (assert (set? val-set)
+          "val-set must be of type clojure.lang.IPersistentSet")
+  (-> (clojure.set/index xrel ks)
+      (assoc selector val-set)
+      (vals-to-flat-coll (empty xrel))))
+
+(defn dissoc-indexed
+  "Returns a new set of relations created by removing the set of
+  relations from xrel that matched the selector when indexed by ks
+  using clojure.set/index."
+  [xrel ks selector]
+  (-> (clojure.set/index xrel ks)
+      (dissoc selector)
+      (vals-to-flat-coll (empty xrel))))
+
+(defn update-indexed
+  "Returns a new set of relations created by updating with function f
+  the set of relations, that match the selector when indexed by ks
+  using clojure.set/index. f must return a set. If the selector
+  doesn't match any existing relations then f is called with nil and
+  the resulting value is added to xrel similarly as with
+  assoc-indexed. If you wish to avoid this return an empty set from f
+  when called with nil."
+  [xrel ks selector f]
+  (-> (clojure.set/index xrel ks)
+      (update selector f)
+      (vals-to-flat-coll (empty xrel))))
+
+(defn update-every-indexed
+  "Returns a new set of relations created by updating with function f
+  every such value in xrel that matches the selector when indexed by
+  ks using clojure.set/index. f must return an updated value for a
+  single relation."
+  [xrel ks selector f]
+  (update-indexed xrel ks selector #(transduce (map f) conj #{} %)))
+
+
+(defn map-values
+  "Update the values in map m by applying function f over them. f takes
+  the old value as the first argument + any supplied args and returns
+  a new value."
+  [m f & args]
+  (if (seq args)
+    (reduce (fn [r [k v]] (assoc r k (apply f v args))) {} m)
+    (reduce (fn [r [k v]] (assoc r k (f v))) {} m)))
+
+(defn map-keys
+  "Update the keys in a map m by applying function f over them. f
+  takes the old value as the first argument + any supplied args and
+  returns a new value."
+  [m f & args]
+  (if (seq args)
+    (reduce (fn [r [k v]] (assoc r (apply f k args) v)) {} m)
+    (reduce (fn [r [k v]] (assoc r (f k) v)) {} m)))
---
+++ test/harmony/util/data_test.clj
@@ -0,0 +1,97 @@
+(ns harmony.util.data-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [harmony.util.data :refer :all]))
+
+(def tr1 {:id 1 :locale "fi" :name "foo"})
+(def tr2 {:id 2 :locale "en" :name "bar"})
+(def tr3 {:id 3 :locale "sv" :name "doo"})
+(def tr4 {:id 4 :locale "en" :name "rab"})
+(def trs #{tr1 tr2 tr3 tr4})
+
+(deftest test-assoc-indexed
+  (is (=
+       #{{:id 1 :locale "fi" :name "foobar"} tr2 tr3 tr4}
+       (assoc-indexed trs [:id] {:id 1} #{{:id 1 :locale "fi" :name "foobar"}}))
+      "Replacing a set on a given indexed value")
+  (is (=
+       #{tr1 tr2 tr3 tr4 {:id 5 :locale "no" :name "barbar"}}
+       (assoc-indexed trs [:id] {:id 5} #{{:id 5 :locale "no" :name "barbar"}}))
+      "Adding an element"))
+
+(deftest test-dissoc-indexed
+  (is (=
+       #{tr2 tr3 tr4}
+       (dissoc-indexed trs [:id] {:id 1}))
+      "Removing values for given indexed value")
+  (is (=
+       #{tr1 tr2 tr3 tr4}
+       (dissoc-indexed trs [:id] {:id 5}))
+      "Removing by selector that doesn't match anything"))
+
+(deftest test-update-indexed
+  (is (=
+       #{tr1
+         {:id 2 :locale "en" :name "bar" :extra "extra"}
+         tr3
+         {:id 4 :locale "en" :name "rab" :extra "extra"}}
+       (update-indexed trs
+                       [:locale]
+                       {:locale "en"}
+                       (fn [xrel] (->> xrel
+                                       (map #(assoc % :extra "extra"))
+                                       set))))
+      "Updating a set of matching values")
+  (is (=
+       #{tr1 tr2 tr3 tr4 :foo}
+       (update-indexed trs [:id] {:id 5} (constantly #{:foo})))
+      "Updating by selector that doesn't match existing vals"))
+
+(deftest test-update-every-indexed
+  (is (=
+       #{tr1
+         {:id 2 :locale "en" :name "bar" :extra "extra"}
+         tr3
+         {:id 4 :locale "en" :name "rab" :extra "extra"}}
+       (update-every-indexed trs
+                       [:locale]
+                       {:locale "en"}
+                       #(assoc % :extra "extra")))
+      "Updating a set of matching values")
+
+  ;; Note, this is different from update-every above because there's
+  ;; no elements matching {:id 5} so the given f is not run.
+  (is (=
+       #{tr1 tr2 tr3 tr4}
+       (update-every-indexed trs [:id] {:id 5} (constantly :foo)))
+      "Updating by selector that doesn't match existing vals"))
+
+(deftest test-all-indexed-preserve-coll-type
+  (is (= [tr2 tr1]
+         (assoc-indexed [tr1] [:id] {:id 2} #{tr2})))
+  (is (= (list tr1 tr2)
+         (assoc-indexed (list tr1) [:id] {:id 2} #{tr2})))
+  (is (= [{:id 1 :locale "fi" :name "foobar"}]
+         (update-every-indexed [tr1] [:id] {:id 1} #(assoc % :name "foobar"))))
+  (is (= (list {:id 1 :locale "fi" :name "foobar"})
+         (update-every-indexed `(~tr1) [:id] {:id 1} #(assoc % :name "foobar"))))
+  (is (= [tr1]
+         (dissoc-indexed [tr1 tr2] [:id] {:id 2})))
+  (is (= (list tr1)
+         (dissoc-indexed (list tr1 tr2) [:id] {:id 2}))))
+
+
+(deftest test-map-values
+  (is (= {:a 3 :b 6}
+         (map-values {:a 1 :b 2} #(* % 3)))
+      "single argument to f")
+
+  (is (= {:a 5 :b 10}
+         (map-values {:a 1 :b 2} #(* %1 %2) 5))
+      "additional arg to f"))
+
+(deftest test-map-keys
+  (is (= {:a 3 :b 6}
+         (map-keys {"a" 3 "b" 6} keyword)))
+
+  (is (= {6 :3 7 :6}
+         (map-keys {1 :3 2 :6} + 5))))
