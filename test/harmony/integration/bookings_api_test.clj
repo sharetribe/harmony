8b3a5a508e006908e95c007533d36dec0fdf409d
Mikko Koski <mikkokos@gmail.com>
2016-09-19 16:15:31 +0300

    Get rid of unnecessary api-response and api-resource validators

--- test/harmony/integration/bookings_api_test.clj
+++ test/harmony/integration/bookings_api_test.clj
@@ -34,21 +34,11 @@
                       (f)
                       (teardown)))
 
-(defn- uuid? [uuid-candidate]
-  (instance? java.util.UUID uuid-candidate))
-
 (defn- contains-many? [m & ks]
   (every? #(contains? m %) ks))
 
 (defn- submap? [submap supermap]
   (= submap (select-keys supermap (keys submap))))
-
-(defn- json-api-response? [body]
-  (contains-many? body :data :meta :included))
-
-(defn- json-api-resource? [{:keys [id] :as body}]
-  (contains-many? body :id :type :attributes :relationships)
-  (uuid? id))
 
 (defn- resource-attrs-include? [expected attrs]
   (submap? expected attrs))
@@ -72,8 +62,6 @@
                                 :accept :transit+msgpack
                                 :content-type :transit+msgpack})]
     (is (= 201 status))
-    (is (json-api-response? body))
-    (is (json-api-resource? (:data body)))
     (is (resource-attrs-include? {:marketplaceId (fixed-uuid :marketplaceId)
                                   :refId (fixed-uuid :refId)
                                   :authorId (fixed-uuid :authorId)
@@ -140,8 +128,6 @@
 
 
     (is (= 201 status))
-    (is (json-api-response? body))
-    (is (json-api-resource? (:data body)))
     (is (resource-attrs-include? {:customerId (fixed-uuid :customerId)
                                   :status :paid
                                   :seats 1
@@ -172,8 +158,6 @@
                                 :accept :transit+msgpack})]
 
     (is (= 200 status))
-    (is (json-api-response? body))
-    (is (every? json-api-resource? (:data body)))
 
     (let [timeslots (map timeslot [#inst "2016-09-19T00:00:00.000Z"
                                    #inst "2016-09-20T00:00:00.000Z"
@@ -206,8 +190,6 @@
 
 
     (is (= 201 status))
-    (is (json-api-response? body))
-    (is (json-api-resource? (:data body)))
     (is (resource-attrs-include? {:customerId (fixed-uuid :customerId)
                                   :status :paid
                                   :seats 1
@@ -231,8 +213,6 @@
 
 
     (is (= 201 status))
-    (is (json-api-response? body))
-    (is (json-api-resource? (:data body)))
     (is (resource-attrs-include? {:customerId (fixed-uuid :customerId)
                                   :status :paid
                                   :seats 1
@@ -250,8 +230,6 @@
                                 :accept :transit+msgpack})]
 
     (is (= 200 status))
-    (is (json-api-response? body))
-    (is (every? json-api-resource? (:data body)))
 
     (let [timeslots (map timeslot [#inst "2016-09-20T00:00:00.000Z"
                                    #inst "2016-09-21T00:00:00.000Z"
